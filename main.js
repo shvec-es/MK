129b8e529a7a687a2d8e9878970e5931d0112cd1
shvec-es <kotova.es.1510@gmail.com>
2021-12-12 14:44:37 +0200

    Работает неправильно, но сейчас не успеваю исправить(

--- main.js
+++ main.js
@@ -1,49 +1,82 @@
+const divArenas = document.querySelector(".arenas");
+const randomBtn = document.querySelector(".button");
+
 const player1 = {
+  player: 1,
   name: "Scorpion",
   hp: 50,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["arbalet", "knife"],
-  attack() {
-    console.log(this.name + " " + "Fight...");
+  attack(name) {
+    console.log(name + " " + "Fight...");
   },
 };
 
 const player2 = {
+  player: 2,
   name: "Sonya",
   hp: 80,
   img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
   weapon: ["sword", "knife"],
-  attack() {
-    console.log(this.name + " " + "Fight...");
+  attack(name) {
+    console.log(name + " " + "Fight...");
   },
 };
 
-const divArenas = document.querySelector(".arenas");
+function createElement(tag, className) {
+  const tagEl = document.createElement(tag);
 
-function createPlayer(player, obj) {
-  const divPlayer1 = document.createElement("div");
-  divPlayer1.classList.add(player);
-  divArenas.appendChild(divPlayer1);
+  if (className) {
+    tagEl.classList.add(className);
+  }
 
-  const divProgressbar = document.createElement("div");
-  divProgressbar.classList.add("progressbar");
-  divPlayer1.appendChild(divProgressbar);
-  const divLife = document.createElement("div");
-  divLife.classList.add("life");
-  divLife.style.width = `${obj.hp}%`;
-  divProgressbar.appendChild(divLife);
-  const divName = document.createElement("div");
-  divName.classList.add("name");
-  divName.innerText = obj.name;
-  divProgressbar.appendChild(divName);
-
-  const divCharacter = document.createElement("div");
-  divCharacter.classList.add("character");
-  divPlayer1.appendChild(divCharacter);
-  const imgEl = document.createElement("img");
-  imgEl.src = obj.img;
-  divCharacter.appendChild(imgEl);
+  return tagEl;
 }
 
-createPlayer("player1", player1);
-createPlayer("player2", player2);
+function createPlayer(obj) {
+  const divPlayer = createElement("div", "player" + obj.player);
+  const divProgressbar = createElement("div", "progressbar");
+  const divLife = createElement("div", "life");
+  const divName = createElement("div", "name");
+  const divCharacter = createElement("div", "character");
+  const imgEl = createElement("img");
+
+  divLife.style.width = `${obj.hp}%`;
+  divName.innerText = obj.name;
+  imgEl.src = obj.img;
+
+  divProgressbar.appendChild(divLife);
+  divProgressbar.appendChild(divName);
+  divCharacter.appendChild(imgEl);
+  divPlayer.appendChild(divProgressbar);
+  divPlayer.appendChild(divCharacter);
+
+  return divPlayer;
+}
+
+function changeHP(player) {
+  const playerLife = document.querySelector(
+    ".player" + player.player + " .life"
+  );
+  player.hp -= 20;
+  playerLife.style.width = `${player.hp}%`;
+
+  if (player.hp < 0) {
+    divArenas.appendChild(playerLose(player.name));
+  }
+}
+
+function playerLose(name) {
+  const loseTitle = createElement("div", "loseTitle");
+  loseTitle.innerText = name + " lose";
+
+  return loseTitle;
+}
+
+randomBtn.addEventListener("click", () => {
+  changeHP(player1);
+  changeHP(player2);
+});
+
+divArenas.appendChild(createPlayer(player1));
+divArenas.appendChild(createPlayer(player2));
